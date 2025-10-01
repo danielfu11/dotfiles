a594f9aba8eb1bdde7ee9aabb0e0ce1c6b61c97d
Daniel Fu <danielfu97@gmail.com>
2025-10-01 10:56:06 -0700

    use catppuccin for theme; also added which-key to nvim (may remove later)

--- nvim/.config/nvim/lua/mangobay/lazy/colorscheme.lua
+++ nvim/.config/nvim/lua/mangobay/lazy/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
   { 
     "Mofiqul/dracula.nvim",
-    config = function(_, opts)
-      require("dracula").setup(opts)
-      vim.cmd.colorscheme("dracula")
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato",
+      })
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
