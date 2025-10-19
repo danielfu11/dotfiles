75e60206b84a52a030f79d57828fa9533fdc88cd
Daniel Fu <danielfu97@gmail.com>
2025-10-19 11:51:34 -0700

    nvim: remap window nav keys; also lazy update

--- nvim/.config/nvim/lua/mangobay/remap.lua
+++ nvim/.config/nvim/lua/mangobay/remap.lua
@@ -9,6 +9,10 @@ vim.keymap.set("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle Fil
 -- Navigation
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Always center after jumping
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to down window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to up window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
 -- Buffers
 vim.keymap.set("n", "<leader>sh", vim.cmd.split)
