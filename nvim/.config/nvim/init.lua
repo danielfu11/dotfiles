b674bacb005fd9f3536a1f410a89da6c307c51e7
Daniel Fu <danielfu97@gmail.com>
2025-10-28 23:21:44 -0700

    nvim: update lsp keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,15 +27,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
     }
 
     -- native neovim keymaps
-    keymap("n", "<leader>gD", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- goto definition
+    keymap("n", "<leader>gd", vim.lsp.buf.definition, opts) -- goto definition
+    keymap("n", "<leader>gD", vim.lsp.buf.declaration, opts) -- goto declaration
+    keymap("n", "<leader>gi", vim.lsp.buf.implementation, opts)
     keymap("n", "<leader>gS", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts) -- goto definition in split
-    keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- Code actions
-    keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts) -- Rename symbol
+    keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- Code actions
+    keymap("n", "<leader>rn", vim.lsp.buf.rename, opts) -- Rename symbol
     keymap("n", "<leader>D", "<cmd>lua vim.diagnostic.open_float({ scope = 'line' })<CR>", opts) -- Line diagnostics (float)
-    keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts) -- Cursor diagnostics
+    keymap("n", "<leader>d", vim.diagnostic.open_float, opts) -- Cursor diagnostics
     keymap("n", "<leader>pd", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts) -- previous diagnostic
     keymap("n", "<leader>nd", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts) -- next diagnostic
-    keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts) -- hover documentation
+    keymap("n", "K", vim.lsp.buf.hover, opts) -- hover documentation
 
     -- fzf-lua keymaps
     keymap("n", "<leader>gd", "<cmd>FzfLua lsp_finder<CR>", opts) -- LSP Finder (definition + references)
