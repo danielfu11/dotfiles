47360dd56d66e19576229f6b2f89946ae6b43045
Daniel Fu <danielfu97@gmail.com>
2025-11-27 13:29:37 -0800

    nvim: use blink-cmp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,14 +18,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     if not client then return end
 
     if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
     end
 
     -- format-on-save
     if not client:supports_method('textDocument/willSaveWaitUntil')
-      and client:supports_method('textDocument/formatting') then
+        and client:supports_method('textDocument/formatting') then
       vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
         buffer = args.buf,
         callback = function()
           vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
@@ -35,23 +35,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     local keymap = vim.keymap.set
     local opts = {
-      noremap = true, -- prevent recursive mapping
-      silent = true, -- don't print the command to the cli
-      buffer = bufnr, -- restrict the keymap to the local buffer number
+      noremap = true,    -- prevent recursive mapping
+      silent = true,     -- don't print the command to the cli
+      buffer = args.buf, -- restrict the keymap to the local buffer number
     }
 
     -- native neovim keymaps
-    keymap("n", "<leader>gd", vim.lsp.buf.definition, opts) -- goto definition
-    keymap("n", "<leader>gD", vim.lsp.buf.declaration, opts) -- goto declaration
+    keymap("n", "<leader>gd", vim.lsp.buf.definition, opts)                                      -- goto definition
+    keymap("n", "<leader>gD", vim.lsp.buf.declaration, opts)                                     -- goto declaration
     keymap("n", "<leader>gi", vim.lsp.buf.implementation, opts)
-    keymap("n", "<leader>gS", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts) -- goto definition in split
-    keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- Code actions
-    keymap("n", "<leader>rn", vim.lsp.buf.rename, opts) -- Rename symbol
+    keymap("n", "<leader>gS", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)            -- goto definition in split
+    keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)                                     -- Code actions
+    keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)                                          -- Rename symbol
     keymap("n", "<leader>D", "<cmd>lua vim.diagnostic.open_float({ scope = 'line' })<CR>", opts) -- Line diagnostics (float)
-    keymap("n", "<leader>d", vim.diagnostic.open_float, opts) -- Cursor diagnostics
-    keymap("n", "<leader>pd", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts) -- previous diagnostic
-    keymap("n", "<leader>nd", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts) -- next diagnostic
-    keymap("n", "K", vim.lsp.buf.hover, opts) -- hover documentation
+    keymap("n", "<leader>d", vim.diagnostic.open_float, opts)                                    -- Cursor diagnostics
+    keymap("n", "<leader>pd", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)                   -- previous diagnostic
+    keymap("n", "<leader>nd", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)                   -- next diagnostic
+    keymap("n", "K", vim.lsp.buf.hover, opts)                                                    -- hover documentation
     -- toggle inlay hints
     if client.server_capabilities.inlayHintProvider then
       keymap("n", "<leader>ti",
@@ -62,13 +62,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 
     -- fzf-lua keymaps
-    keymap("n", "<leader>gd", "<cmd>FzfLua lsp_finder<CR>", opts) -- LSP Finder (definition + references)
-    keymap("n", "<leader>gr", "<cmd>FzfLua lsp_references<CR>", opts) -- Show all references to the symbol under the cursor
-    keymap("n", "<leader>gt", "<cmd>FzfLua lsp_typedefs<CR>", opts) -- Jump to the type definition of the symbol under the cursor
-    keymap("n", "<leader>ds", "<cmd>FzfLua lsp_document_symbols<CR>", opts) -- List all symbols (functions, classes, etc.) in the current file
+    keymap("n", "<leader>gd", "<cmd>FzfLua lsp_finder<CR>", opts)            -- LSP Finder (definition + references)
+    keymap("n", "<leader>gr", "<cmd>FzfLua lsp_references<CR>", opts)        -- Show all references to the symbol under the cursor
+    keymap("n", "<leader>gt", "<cmd>FzfLua lsp_typedefs<CR>", opts)          -- Jump to the type definition of the symbol under the cursor
+    keymap("n", "<leader>ds", "<cmd>FzfLua lsp_document_symbols<CR>", opts)  -- List all symbols (functions, classes, etc.) in the current file
     keymap("n", "<leader>ws", "<cmd>FzfLua lsp_workspace_symbols<CR>", opts) -- Search for any symbol across the entire project/workspace
-    keymap("n", "<leader>gi", "<cmd>FzfLua lsp_implementations<CR>", opts) -- Go to implementation
-
+    keymap("n", "<leader>gi", "<cmd>FzfLua lsp_implementations<CR>", opts)   -- Go to implementation
   end,
 })
-
--- nvim/.config/nvim/lua/mangobay/lazy/init.lua
+++ nvim/.config/nvim/lua/mangobay/lazy/init.lua
@@ -1,5 +1,5 @@
 -- All the one-liner, no-config-required plugins are managed here
 return {
-  { "mason-org/mason.nvim", opts = {} }, -- Mason as a package manager for LSPs, DAPs, etc.
+  { "mason-org/mason.nvim",        opts = {} }, -- Mason as a package manager for LSPs, DAPs, etc.
   { "nvim-tree/nvim-web-devicons", opts = {} },
 }
