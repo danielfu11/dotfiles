71290ef4da484bad0673663d3bfcd5e252a286f8
Daniel Fu <danielfu97@gmail.com>
2025-11-27 12:45:34 -0800

    nvim: setup lua lsp

--- nvim/.config/nvim/lua/mangobay/lazy/lsp.lua
+++ nvim/.config/nvim/lua/mangobay/lazy/lsp.lua
@@ -1,5 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      }
+    }
+  },
   config = function()
     -- specific overrides for configs
     vim.lsp.config.clangd = {
@@ -9,10 +22,14 @@ return {
         "--header-insertion=never",
       }
     }
+    vim.lsp.config('rust_analyzer', {})
+    vim.lsp.config('basedpyright', {})
+    vim.lsp.config('lua_ls', {})
     vim.lsp.enable({
       'clangd',
       'rust_analyzer',
       'basedpyright',
+      'lua_ls',
     })
   end
 }
